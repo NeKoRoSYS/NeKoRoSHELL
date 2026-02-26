a46d6844250418b1f1a6a210007e8d7d502f9bfb
NeKoRoSYS <nekorosys@gmail.com>
2026-02-26 22:14:12 +0800

    Update random.sh

--- .config/hypr/scripts/wallpapers/random.sh
+++ .config/hypr/scripts/wallpapers/random.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WALLPAPERS_DIR="$XDG_CONFIG_HOME/wallpapers"
+WALLPAPERS_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/wallpapers"
 NEW_WALL=$(find $WALLPAPERS_DIR -type f | shuf -n 1)
 
 echo $NEW_WALL
